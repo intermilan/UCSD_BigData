510a3fec5d966e1fcd3ddcc1e5e730d6b50f8721
Long Jin <roy.kim.thu@gmail.com>
2014-05-13 12:12:09 -0700

    Delete AWSCredentials.py

--- LocalScripts/AWSCredentials.py
+++
@@ -1,7 +0,0 @@
-### AWS credentials: ####
-aws_access_key_id='AKIAIMVEMIEP25S3FTIQ'
-aws_secret_access_key='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'
-keyPairFile="/Users/jinlong/Documents/cse291.pem" # name of file keeping local key
-key_name="cse291" # name of keypair (not name of file where key is stored)
-security_groups=['default']
-### End of AWS credentials ####
